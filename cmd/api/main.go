16a937aae8fdf5fba3844b1f9c5018c4c8b9cbdf
mhieu100 <hieunguyen201103@gmail.com>
2025-12-27 15:40:22 +0700

    refactor: reorganize imports and add error handling for .env loading

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,37 +1,48 @@
 package main
 
 import (
-    "net/http"
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
 )
 
 type Book struct {
-    ID     string  `json:"id"`
-    Title  string  `json:"title"`
-    Author string  `json:"author"`
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
 }
 
 var books = []Book{
-    {ID: "1", Title: "Golang cơ bản", Author: "Gemini"},
-    {ID: "2", Title: "Học Microservices", Author: "Google"},
+	{ID: "1", Title: "Golang cơ bản", Author: "Gemini"},
+	{ID: "2", Title: "Học Microservices", Author: "Google"},
 }
 
 func main() {
-    router := gin.Default()
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
+	router := gin.Default()
 	router.SetTrustedProxies([]string{"127.0.0.1"})
 
-    router.GET("/books", func(c *gin.Context) {
-        c.IndentedJSON(http.StatusOK, books)
-    })
+	router.GET("/books", func(c *gin.Context) {
+		c.IndentedJSON(http.StatusOK, books)
+	})
 
-    router.POST("/books", func(c *gin.Context) {
-        var newBook Book
-        if err := c.BindJSON(&newBook); err != nil {
-            return
-        }
-        books = append(books, newBook)
-        c.IndentedJSON(http.StatusCreated, newBook)
-    })
+	router.POST("/books", func(c *gin.Context) {
+		var newBook Book
+		if err := c.BindJSON(&newBook); err != nil {
+			return
+		}
+		books = append(books, newBook)
+		c.IndentedJSON(http.StatusCreated, newBook)
+	})
 
-    router.Run("localhost:8080")
+	router.Run("localhost:" + port)
 }
